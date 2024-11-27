c84c23a1ea59ae7b48caa3aff3536799fc8b16ae
ANGELfer10 <complejovilladelosmariscales@gmail.com>
2024-11-27 02:42:27 +0100

    Update forms.py

--- capp/users/forms.py
+++ capp/users/forms.py
@@ -3,18 +3,18 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-  username = StringField('Username', 
+  username = StringField('Nombre de usuario', 
                          validators=[DataRequired(), Length(min=2, max=30)])
   email = StringField('Email',
                       validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  confirm_password = PasswordField('Confirm Password',
+  password = PasswordField('Contraseña', validators=[DataRequired()])
+  confirm_password = PasswordField('Confirma contraseña',
                       validators=[DataRequired(), EqualTo('password')])                                
-  submit = SubmitField('Sign Up')
+  submit = SubmitField('Incríbete')
 
 class LoginForm(FlaskForm):
   email = StringField('Email',
                       validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  remember = BooleanField('Remember Me')
-  submit = SubmitField('Login') 
+  password = PasswordField('Contraseña', validators=[DataRequired()])
+  remember = BooleanField('Recuérdamelo')
+  submit = SubmitField('Acceso') 
